efbb89004cc1a2c9ed3f43276e3097a4af5ce53b
Subu Kaundinya <subu.kaundinya@gmail.com>
2020-06-05 18:08:11 -0400

    New Chinese ethnic project

---
+++ Ethnicity_in_China/Ananta_Ethnic_Groups_in_China.js
@@ -0,0 +1,14 @@
+
+
+function names(arg1) {
+	document.getElementById("names").innerHTML=arg1;
+	//console.log(arg1);
+}
+
+function congrats(arg1) {
+	document.getElementById("congrats").innerHTML=arg1;
+
+}
+
+
+
