19c37e54bc9e526470d21b5eee095211083e34f6
Subu Kaundinya <subu.kaundinya@gmail.com>
2020-08-11 07:45:38 -0400

    I made a quiz

---
+++ Krishna's-treasure/script.js
@@ -0,0 +1,9 @@
+function janmashtami(){
+    alert ("HAPPY JANMASHTAMI!!!")
+}
+janmashtami()
+
+function alert(arg1){
+    document.getElementById("alert").innerHTML=arg1;
+    console.log(arg1);
+}
